dbbdb26055cb38d2b6991b6d47febe67f03a4337
OutcastR97 <sadman.rashiq.dbz@gmail.com>
2023-02-18 18:43:21 +0600

    footer-design

---
+++ frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <div className="footer__lists">
+                <ul className="footer__list">
+                    <li className="footer__listItem">Countries</li>
+                    <li className="footer__listItem">Regions</li>
+                    <li className="footer__listItem">Cities</li>
+                    <li className="footer__listItem">Districts</li>
+                    <li className="footer__listItem">Airports</li>
+                    <li className="footer__listItem">Hotels</li>
+                    <li className="footer__listItem">Places of interest</li>
+                </ul>
+                <ul className="footer__list">
+                    <li className="footer__listItem">Homes</li>
+                    <li className="footer__listItem">Apartments</li>
+                    <li className="footer__listItem">Resorts</li>
+                    <li className="footer__listItem">Villas</li>
+                    <li className="footer__listItem">Hostels</li>
+                    <li className="footer__listItem">B&Bs</li>
+                    <li className="footer__listItem">Guest houses</li>
+                </ul>
+                <ul className="footer__list">
+                    <li className="footer__listItem">Unique places to stay</li>
+                    <li className="footer__listItem">All destinations</li>
+                    <li className="footer__listItem">All flight destinations</li>
+                    <li className="footer__listItem">Discover</li>
+                    <li className="footer__listItem">Reviews</li>
+                    <li className="footer__listItem">Discover monthly stays</li>
+                    <li className="footer__listItem">Unpacked: Travel articles</li>
+                    <li className="footer__listItem">Travel Communities</li>
+                    <li className="footer__listItem">Seasonal and holiday deals</li>
+                    <li className="footer__listItem">Traveller Review Awards</li>
+                </ul>
+                <ul className="footer__list">
+                    <li className="footer__listItem">Car hire</li>
+                    <li className="footer__listItem">Flight finder</li>
+                    <li className="footer__listItem">Restaurant reservations</li>
+                    <li className="footer__listItem">Booking.com for Travel Agents</li>
+                </ul>
+                <ul className="footer__list">
+                    <li className="footer__listItem">Coronavirus (COVID-19) FAQs</li>
+                    <li className="footer__listItem">About Booking.com</li>
+                    <li className="footer__listItem">Customer Service help</li>
+                    <li className="footer__listItem">Partner help</li>
+                    <li className="footer__listItem">Careers</li>
+                    <li className="footer__listItem">Sustainability</li>
+                    <li className="footer__listItem">Press centre</li>
+                    <li className="footer__listItem">Safety resource centre</li>
+                    <li className="footer__listItem">Investor relations</li>
+                    <li className="footer__listItem">Terms & Conditions</li>
+                    <li className="footer__listItem">Partner dispute</li>
+                    <li className="footer__listItem">How we work</li>
+                    <li className="footer__listItem">Privacy & Cookie Statement</li>
+                    <li className="footer__listItem">MSA Statement</li>
+                    <li className="footer__listItem">Corporate contact</li>
+                </ul>
+            </div>
+            <div className="footer__text">
+                Copyright © 1996–2023 bookingDotCom™. All rights reserved.
+            </div>
+        </div>
+    )
+}
+
+export default Footer
