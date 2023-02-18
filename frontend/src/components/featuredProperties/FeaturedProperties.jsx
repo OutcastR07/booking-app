dff433d5e1999b55e147ed6660bce53f4d170e1d
OutcastR97 <sadman.rashiq.dbz@gmail.com>
2023-02-18 16:01:40 +0600

    homepage-lists

---
+++ frontend/src/components/featuredProperties/FeaturedProperties.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import './FeaturedProperties.css'
+
+const FeaturedProperties = () => {
+    return (
+        <div className='featured__properties'>
+            <div className="featured__propertiesItem">
+                <img src="https://images.trvl-media.com/hotels/11000000/10580000/10578000/10577932/649b7896_z.jpg" alt="" className="featured__propertiesImg" />
+                <span className='featured__propertiesName'>Platinum Residence</span>
+                <span className='featured__propertiesCity'>Dhaka</span>
+                <span className='featured__propertiesPrice'>Starting from Tk. 8,030</span>
+                <div className="featured__propertiesRating">
+                    <button>8.0</button>
+                    <span>Excellent</span>
+                </div>
+            </div>
+            <div className="featured__propertiesItem">
+                <img src="https://images.trvl-media.com/hotels/11000000/10580000/10578000/10577932/649b7896_z.jpg" alt="" className="featured__propertiesImg" />
+                <span className='featured__propertiesName'>Platinum Residence</span>
+                <span className='featured__propertiesCity'>Dhaka</span>
+                <span className='featured__propertiesPrice'>Starting from Tk. 8,030</span>
+                <div className="featured__propertiesRating">
+                    <button>8.0</button>
+                    <span>Excellent</span>
+                </div>
+            </div>
+            <div className="featured__propertiesItem">
+                <img src="https://images.trvl-media.com/hotels/11000000/10580000/10578000/10577932/649b7896_z.jpg" alt="" className="featured__propertiesImg" />
+                <span className='featured__propertiesName'>Platinum Residence</span>
+                <span className='featured__propertiesCity'>Dhaka</span>
+                <span className='featured__propertiesPrice'>Starting from Tk. 8,030</span>
+                <div className="featured__propertiesRating">
+                    <button>8.0</button>
+                    <span>Excellent</span>
+                </div>
+            </div>
+            <div className="featured__propertiesItem">
+                <img src="https://images.trvl-media.com/hotels/11000000/10580000/10578000/10577932/649b7896_z.jpg" alt="" className="featured__propertiesImg" />
+                <span className='featured__propertiesName'>Platinum Residence</span>
+                <span className='featured__propertiesCity'>Dhaka</span>
+                <span className='featured__propertiesPrice'>Starting from Tk. 8,030</span>
+                <div className="featured__propertiesRating">
+                    <button>8.0</button>
+                    <span>Excellent</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FeaturedProperties
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import Featured from '../../components/featured/Featured'
+import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties'
 import Header from '../../components/header/Header'
 import Navbar from '../../components/navbar/Navbar'
+import PropertyList from '../../components/propertyList/PropertyList'
 import './Home.css'
 
 const Home = () => {
@@ -8,6 +11,13 @@ const Home = () => {
         <div>
             <Navbar />
             <Header />
+            <div className="home__container">
+                <Featured />
+                <h1 className="home__title">Browse by property type</h1>
+                <PropertyList />
+                <h1 className="home__title">Popular homes</h1>
+                <FeaturedProperties />
+            </div>
         </div>
     )
 }
