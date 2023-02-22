8d93897e2ab3e336512fe272e5a79166e1e9e2b8
OutcastR97 <sadman.rashiq.dbz@gmail.com>
2023-02-22 07:51:44 +0600

    error-handling

--- backend/routes/hotels.js
+++ backend/routes/hotels.js
@@ -1,5 +1,6 @@
 import express from "express";
 import Hotel from "../models/Hotel.js"
+import { createError } from "../utils/error.js";
 
 const router = express.Router();
 
@@ -43,12 +44,13 @@ router.get("/:id", async (req, res) => {
     }
 });
 // Get All
-router.get("/", async (req, res) => {
+router.get("/", async (req, res, next) => {
+
     try {
-        const hotel = await Hotel.find(req.params.id);
-        res.status(200).json(hotel);
+        const hotels = await Hotel.find();
+        res.status(200).json(hotels);
     } catch (err) {
-        res.status(500).json(err);
+        next(err);
     }
 });
 
